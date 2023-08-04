bc6dd5219efa2ae92a1fd26346868046ca19dfd0
mdrobot3115 <116418622+mdrobot3115@users.noreply.github.com>
2023-08-04 22:25:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,11 +5,11 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.private & filters.command(["start"]) & filters.incoming)
 async def start(client, message):
     btn = [[
-        InlineKeyboardButton('Updates Channel', url='https://t.me/SL_Bots_Updates'),
-        InlineKeyboardButton('Support Group', url='https://t.me/SL_Bots_Support')
+        InlineKeyboardButton('Updates Channel', url=''),
+        InlineKeyboardButton('Support Group', url='')
     ],[
-        InlineKeyboardButton('Repo', url='https://github.com/Hansaka-Anuhas/ForwardBot'),
-        InlineKeyboardButton('Developer', url='https://t.me/Hansaka_Anuhas')
+        InlineKeyboardButton('Repo', url=''),
+        InlineKeyboardButton('Developer', url='')
     ]]
     text = """I can forward document and video (mp4 and mkv) files.
 

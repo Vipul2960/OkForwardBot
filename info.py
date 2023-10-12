c8e28035af66bb2770dfddcbd2e1cf275ed2ab12
Vipul2960 <109511098+Vipul2960@users.noreply.github.com>
2023-10-12 10:54:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', '9844066'))
-API_HASH = environ.get('API_HASH', '0d3f74056f1e60388d3317548799ee17')
-BOT_TOKEN = environ.get('BOT_TOKEN', '5757059757:AAEvjvpv1gmn8xHGAONAiTEjEbN7OaHwka8')
+API_ID = int(environ.get('API_ID', '10039364'))
+API_HASH = environ.get('API_HASH', '63f5e9ca5613f8830cb8c3441ea04c01')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6027926807:AAGVyM_qaA-9IWIg5V-bnrhTcmUv5ZCjFbU')
 FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
-OWNER = int(environ.get('OWNER', '1770455672'))
+OWNER = int(environ.get('OWNER', '1032473392'))
 PRIVATE_BOT = bool(environ.get('PRIVATE_BOT', False))
 #==
